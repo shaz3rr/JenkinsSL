a6819aeb9127c2a608e36ca44bc17928b909123f
M1GHTY <shaz3rr@gmail.com>
2022-10-13 16:54:30 +0100

    folder fix

---
+++ src/com/exaple/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+package com.exaple
+
+class Docker implements Serializable {
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin localhost:8083"
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
+import com.exaple.Docker
+
 def call(String imageName) {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        sh "echo $PASS | docker login -u $USER --password-stdin localhost:8083"
-        sh "docker push $imageName"
+    return new Docker(this).buildDockerImage(imageName)
     }
-}
